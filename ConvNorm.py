8f97f55098cfb692e0c74c4cfc8fbf5ef1a7a724
tpapp157 <tpapp157@users.noreply.github.com>
2020-03-24 13:44:18 -0400

    Initial commit.

---
+++ ConvNorm.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow.keras import backend as K
+from tensorflow.keras import initializers, regularizers, constraints
+from tensorflow.keras.layers import Layer, InputSpec
+from tensorflow.python.keras.utils import conv_utils
+import tensorflow as tf
+
+
+
+class ConvNorm(Layer):
+
+    def __init__(self, filters, kernel_size=3, strides=1, kernel_initializer='glorot_uniform', **kwargs):
+        super(ConvNorm, self).__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, 2, 'kernel_size')
+        self.strides = strides
+        self.kernel_initializer = initializers.get(kernel_initializer)
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+        self.kernel = self.add_weight(shape=kernel_shape, initializer=self.kernel_initializer, name='kernel')
+        self.built = True
+
+    def call(self, inputs):
+        weights = self.kernel
+        
+        x = inputs
+
+        #Normalize
+        d = K.sqrt(K.sum(K.square(weights), axis=[0,1,2], keepdims=True) + 1e-8)
+        weights = weights / d
+        
+        if self.kernel_size[0]>1:
+            p = (self.kernel_size[0]-1)//2
+            x = tf.pad(x, [[0,0], [p,p], [p,p], [0,0]], mode='SYMMETRIC')
+        
+        x = tf.nn.conv2d(x, weights, strides=self.strides, padding="VALID")
+        return x
+    
+    def compute_output_shape(self, input_shape):
+        return input_shape[:3] + self.out_channels
+    
+    def get_config(self):
+        config = {
+            'filters': self.filters,
+            'kernel_size': self.kernel_size,
+            'strides': self.strides,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer)
+        }
+        base_config = super(ConvNorm, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
---
+++ viewFeatures.py
@@ -0,0 +1,50 @@
+
+from sklearn.decomposition import PCA
+
+
+#%%
+def model():
+    temp = Encoders[1]
+    outputs = [temp.get_layer(index=i).output for i in [j for j,n in enumerate(temp.layers) if ('add' in n.name)]]
+    return tf.keras.Model([temp.input], outputs)
+
+M = model()
+
+
+#%%
+inp = []
+test = []
+for _,n,_,_ in train_dataset.take(25):
+    test.append([np.array(i) for i in M(n)])
+    inp.append(np.array(n))
+inp = np.vstack(inp)
+
+out = [[] for _ in range(len(test[0]))]
+for i in test:
+    for n,j in enumerate(i):
+        out[n].append(j)
+test = [np.vstack(i) for i in out]
+#%%
+N = 9
+F = 5
+im = ((np.squeeze(np.concatenate(np.split(inp[:N,:,:,::-1], N), axis=2))+1)*127.5).astype('uint8')
+IM = np.tile(im, (F,1,1))
+cv2.imshow('im0',im)
+
+#%%
+x = test[3]
+P = PCA(n_components=F).fit(np.reshape(x,(-1,x.shape[-1])))
+print(P.explained_variance_ratio_)
+c = P.components_
+
+x1 = np.reshape(P.transform(np.reshape(x,(-1,x.shape[-1]))), list(x.shape[:3])+[F])
+
+#%%
+#i = np.arange(F)
+i = np.argsort(x.std(axis=(0,1,2)))[-5:]
+mask = np.concatenate(np.split(x[:N,:,:,i], N), axis=2)
+mask = np.concatenate(np.split(mask, len(i), axis=3), axis=1)
+mask = (mask-mask.min())/(mask.max()-mask.min())
+mask = np.repeat(np.repeat(mask, 256//x.shape[1], 1), 256//x.shape[2], 2)[0,:,:,:]
+
+cv2.imshow('im',(IM.astype('float')*mask).astype('uint8'))
